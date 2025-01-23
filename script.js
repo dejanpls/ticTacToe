263d51018e2932b6269d8e07019d63fe253c79a9
dejanpls <68787353+dejanpls@users.noreply.github.com>
2025-01-23 18:33:47 +0100

    feat: add utility to get a random available spot on the game board

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 const getGameBoard = () => [...Array(9).keys()].map(i => null); // make a 3 * 3 table
 
+const getRandomIndex = (spots) => {
+    const indexes = spots.map((val, i) => (val === null ? i : null)).filter(i => i !== null)
+
+    if (indexes.length > 0) {
+        const randomIndex = Math.floor(Math.random() * indexes.length);
+        return indexes[randomIndex];
+    } else {
+        return -1; // no null values
+    }
+}
+
 const gameBoard = (function () {
     const spots = getGameBoard();
 
     const selectSpot = (spot) => {
         if (spots[spot] === null) {
             spots[spot] =  "X";
+
+            const randomIndex = getRandomIndex(spots);
+
+            if (randomIndex !== -1) spots[randomIndex] = "O";
         }
     }
 

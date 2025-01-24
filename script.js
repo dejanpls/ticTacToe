531d08008cc4a12b494815e46e37e0bed6e86e1f
dejanpls <68787353+dejanpls@users.noreply.github.com>
2025-01-24 19:57:31 +0100

    feat: add function to check winning condition

--- script.js
+++ script.js
@@ -11,22 +11,67 @@ const getRandomIndex = (spots) => {
     }
 }
 
+function createPlayer (marker) {    
+
+    const getMarker = () => marker;
+    const switchMarker = () => marker = marker === "X" ? "O" : "X";
+
+    return {getMarker, switchMarker};
+}
+
 const gameBoard = (function () {
-    const spots = getGameBoard();
+    let spots = getGameBoard();
+    let wins = [0, 0];
+
+    let player = createPlayer("X");
+    let computer = createPlayer("O");
 
     const selectSpot = (spot) => {
-        if (spots[spot] === null) {
-            spots[spot] =  "X";
+
+        if (spots[spot] === null && !checkResult(spots)) {
+            spots[spot] =  player.getMarker();
 
             const randomIndex = getRandomIndex(spots);
 
-            if (randomIndex !== -1) spots[randomIndex] = "O";
+            if (checkResult(spots)) {
+                console.log("You won!");   
+                wins[0] += 1;   
+            } else if (randomIndex !== -1) {
+                spots[randomIndex] = computer.getMarker();
+
+                if (checkResult(spots)) {
+                    console.log("Computer Won!");
+                    wins[1] += 1;
+                }
+            };
+            
+            if (randomIndex === -1 && !checkResult(spots)) console.log("Tie.");
+
+            console.log(spots);
+        }
+    }
+    const reset = () => spots = getGameBoard();
+
+    return {selectSpot, reset, wins};
+})();
+
+const checkResult = (board) => {
+    const winningLines = [
+        [0, 1, 2], // Top row
+        [3, 4, 5], // Middle row
+        [6, 7, 8], // Bottom row
+        [0, 3, 6], // Left column
+        [1, 4, 7], // Middle column
+        [2, 5, 8], // Right column
+        [0, 4, 8], // Top-left to bottom-right diagonal
+        [2, 4, 6]  // Top-right to bottom-left diagonal
+    ];
+
+    for (const [a, b, c] of winningLines) {
+        if (board[a] !== null && board[a] === board[b] && board[b] === board[c]) {
+            return true;
         }
     }
 
-    const getSpots = () => spots;
-
-    const reset = () => getGameBoard();
-
-    return {getSpots, selectSpot, reset};
-})();
+    return false;
+};
